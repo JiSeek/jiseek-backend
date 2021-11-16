b1316edfb7bf0e38c51510d70830408997ff475b
combiJihoon <combi_jihoon@naver.com>
2021-11-16 16:37:37 +0900

    feat: Create Profile model with an imagefield to save an image resized and saved with a method 'upload_image'

--- users/models.py
+++ users/models.py
@@ -2,12 +2,13 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 from .managers import CustomUserManager
 
+from PIL import Image
+
 
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=30)
     nickname = models.CharField(max_length=30, unique=True)
-    # avatar = models.ImageField(upload_to=upload_image, editable=True, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     is_korean = models.BooleanField(default=False)
     is_social_user = models.BooleanField(default=False)
@@ -18,3 +19,28 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    image = models.ImageField(upload_to=upload_image, editable=True, null=True)
+
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_image, editable=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} profile"
+
+    # Override the save method of the model
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)  # Open image
+
+        # resize image
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)  # Resize image
+            img.save(self.image.path)  # Save it again and override the larger image
