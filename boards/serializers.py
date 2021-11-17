195261dc5f8475d06bcb955cfa2d6b2c501dd5ed
jh <kojh0111@naver.com>
2021-11-17 21:39:17 +0900

    feat: Board Serializer

---
+++ boards/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Board
+
+
+class BoardSerializer(ModelSerializer):
+    class Meta:
+        model = Board
+        fields = "__all__"
+        read_only_fields = ("id", "created", "modified", "user")
