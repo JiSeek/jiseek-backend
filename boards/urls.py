6690ff39fc458dba7348cbf312dad0f4482bdcfc
jh <kojh0111@naver.com>
2021-11-19 18:18:31 +0900

    feat: Comment View

--- boards/urls.py
+++ boards/urls.py
@@ -6,4 +6,5 @@ app_name = "boards"
 urlpatterns = [
     path("", views.BoardsView.as_view()),
     path("<int:pk>/", views.BoardView.as_view()),
+    path("<int:pk>/comments/", views.CommentView.as_view()),
 ]
