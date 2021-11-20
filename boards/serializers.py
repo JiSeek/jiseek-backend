4adaa33c4b6e22591c0dd224515ad24e201e9b97
jh <kojh0111@naver.com>
2021-11-20 17:42:19 +0900

    feat: Comment Update and Delete View

--- boards/serializers.py
+++ boards/serializers.py
@@ -6,7 +6,7 @@ class CommentSerializer(ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
-        read_only_fields = ("id", "created", "modified", "user")
+        read_only_fields = ("id", "created", "modified", "board", "user")
 
 
 class BoardSerializer(ModelSerializer):
