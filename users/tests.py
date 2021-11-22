c26487ad57dfdd51c870acae006360cb36aca917
combiJihoon <combi_jihoon@naver.com>
2021-11-22 22:55:26 +0900

    test: Create user test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
+from .models import User
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+client = Client()
+
+
+class RegisterTest(TestCase):
+    def setUp(self):
+        self.User = get_user_model()
+
+    def test_register_post_success(self):
+        register_info = {
+            "email": "test@test.io",
+            "name": "test",
+            "password1": "qwer123!!!",
+            "password2": "qwer123!!!",
+            "is_korean": 0,
+        }
+        response = client.post(
+            "/api/v1/user/register/",
+            json.dumps(register_info),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_register_post_invalid_password(self):
+        register_info = {
+            "email": "test@test.io",
+            "name": "test",
+            "password1": "1234567",
+            "password2": "1234567",
+            "is_korean": 1,
+        }
+        response = client.post(
+            "/api/v1/user/register/",
+            json.dumps(register_info),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_register_post_invalid_email(self):
+        register_info = {
+            "email": "test@testio",
+            "name": "test",
+            "password1": "test123!!!",
+            "password2": "test123!!!",
+            "is_korean": 1,
+        }
+        response = client.post(
+            "/api/v1/user/register/",
+            json.dumps(register_info),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 400)
