0b4e7d9ed85ea5a00b2a424e496e041f4bb7545e
combiJihoon <combi_jihoon@naver.com>
2021-11-22 22:55:07 +0900

    feat: Create mypage with 3 main functions(update profile, toggle favs, show the lists of liked)

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
 from . import views
 from config import settings
 
+
 urlpatterns = [
-    path("", views.ProfileAPI.as_view()),
+    path("board-favs/", views.BoardFavsAPI.as_view()),
+    path("food-favs/", views.FoodFavsAPI.as_view()),
+    path("profile/", views.ProfileAPI.as_view()),
+    path("board/like/<int:pk>/", views.like_board),
+    path("food/like/<int:pk>/", views.like_food),
 ]
 
 
