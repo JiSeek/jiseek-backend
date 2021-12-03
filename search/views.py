89ded0b0ca6b968acd360a6c09b2a2e1484c4ddb
jh <kojh0111@naver.com>
2021-12-04 02:41:50 +0900

    feat: Add Search App to config

--- search/views.py
+++ search/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
+from .models import SearchResult
 
 # Create your views here.
+class SearchResultsView(APIView):
+    # def get_result(self, pk):
+    #     try:
+    #         results = SearchResult.objects.filter(pk=pk)
+    #         if comment.board.id == board_pk:
+    #             return results
+    #         else:
+    #             return None
+    #     except Comment.DoesNotExist:
+    #         return None
+
+    # def get(self, request, pk]):
+    #     results = self.get_comment(pk)
+    #     if comment:
+    #         serializer = CommentSerializer(comment)
+    #         return Response(serializer.data)
+    #     else:
+    #         return Response(status=HTTP_404_NOT_FOUND)
