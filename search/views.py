680055858b4f6bcd423c5b519e9b0b541e695f54
jh <kojh0111@naver.com>
2021-12-06 20:51:50 +0900

    feat: Search View

--- search/views.py
+++ search/views.py
@@ -1,24 +1,22 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.status import HTTP_404_NOT_FOUND
-from .models import SearchResult
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from search.serializers import SearchImageSerializer, SearchResultSerializer
+from .models import SearchImage, SearchResult
 
-# Create your views here.
-class SearchResultsView(APIView):
-    # def get_result(self, pk):
-    #     try:
-    #         results = SearchResult.objects.filter(pk=pk)
-    #         if comment.board.id == board_pk:
-    #             return results
-    #         else:
-    #             return None
-    #     except Comment.DoesNotExist:
-    #         return None
 
-    # def get(self, request, pk]):
-    #     results = self.get_comment(pk)
-    #     if comment:
-    #         serializer = CommentSerializer(comment)
-    #         return Response(serializer.data)
-    #     else:
-    #         return Response(status=HTTP_404_NOT_FOUND)
+class SearchImageView(CreateAPIView):
+    queryset = SearchImage.objects.all()
+    serializer_class = SearchImageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class SearchResultView(ListAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = SearchResultSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs["pk"]
+        return SearchResult.objects.filter(photo_id=pk)
