6ad64452d663468094d453fb94281d5bf8b7f02f
combiJihoon <combi_jihoon@naver.com>
2021-12-07 17:59:45 +0900

    debug: Change the format of the image field when a user logged in

--- users/views.py
+++ users/views.py
@@ -56,7 +56,7 @@ class CustomLoginView(LoginView):
         }
         orginal_response.data.update(expires_at_data)
         orginal_response.data["user"]["name"] = self.user.name
-        orginal_response.data["user"]["image"] = self.user.profile.image
+        orginal_response.data["user"]["image"] = str(self.user.profile.image)
         orginal_response.data["social_platform"] = self.user.social_platform
 
         return orginal_response
@@ -70,7 +70,7 @@ class GoogleLoginView(View):
         response = requests.get(url + token)  # 구글에 id_token을 보내 디코딩 요청
         user = response.json()  # 유저의 정보를 json화해서 변수에 저장
 
-        if User.objects.filter(email=user["email"]).exits():
+        if User.objects.filter(email=user["email"]).exists():
             return JsonResponse({"detail": "The email already exits"}, status=400)
 
         elif User.objects.filter(
@@ -92,7 +92,7 @@ class GoogleLoginView(View):
                         "email": user_info.email,
                         "name": user_info.name,
                         "pk": user_info.id,
-                        "image": user_info.profile.image,
+                        "image": str(user_info.profile.image),
                         "social_platform": user_info.social_platform,
                     },
                     "expires_at": int(round(expires_at.timestamp())),
@@ -117,7 +117,7 @@ class GoogleLoginView(View):
                         "email": new_user_info.email,
                         "name": new_user_info.name,
                         "pk": new_user_info.id,
-                        "image": new_user_info.profile.image,
+                        "image": str(new_user_info.profile.image),
                         "social_platform": new_user_info.social_platform,
                     },
                     "expires_at": timezone.now()
@@ -157,7 +157,7 @@ class KakaoLoginView(View):  # 카카오 로그인
                         "email": user_info.email,
                         "name": user_info.name,
                         "pk": user_info.id,
-                        "image": user_info.profile.image,
+                        "image": str(user_info.profile.image),
                         "social_platform": user_info.social_platform,
                     },
                     "expires_at": int(round(expires_at.timestamp())),
@@ -184,7 +184,7 @@ class KakaoLoginView(View):  # 카카오 로그인
                         "email": new_user_info.email,
                         "name": new_user_info.name,
                         "pk": new_user_info.id,
-                        "image": new_user_info.profile.image,
+                        "image": str(new_user_info.profile.image),
                         "social_platform": new_user_info.social_platform,
                     },
                     "expires_at": timezone.now()
@@ -205,7 +205,7 @@ class NaverLoginView(View):  # 네이버 로그인
         )  # API를 요청하여 회원의 정보를 response에 저장
         user = response.json()
 
-        if User.objects.filter(email=user["response"]["email"]).exits():
+        if User.objects.filter(email=user["response"]["email"]).exists():
             return JsonResponse({"detail": "The email already exits"}, status=400)
 
         elif User.objects.filter(
@@ -223,7 +223,7 @@ class NaverLoginView(View):  # 네이버 로그인
                         "email": user_info.email,
                         "name": user_info.name,
                         "pk": user_info.id,
-                        "image": user_info.profile.image,
+                        "image": str(user_info.profile.image),
                         "social_platform": user_info.social_platform,
                     },
                     "expires_at": timezone.now()
@@ -249,7 +249,7 @@ class NaverLoginView(View):  # 네이버 로그인
                         "email": new_user_info.email,
                         "name": new_user_info.name,
                         "pk": new_user_info.id,
-                        "image": new_user_info.profile.image,
+                        "image": str(new_user_info.profile.image),
                         "social_platform": new_user_info.social_platform,
                     },
                     "expires_at": timezone.now()
@@ -308,6 +308,7 @@ class CustomVerifyEmailView(VerifyEmailView):
                 "user": {
                     "email": self.user.email,
                     "name": self.user.name,
+                    "image": str(user.profile.image),
                     "pk": self.user.id,
                 },
                 "expires_at": timezone.now()
