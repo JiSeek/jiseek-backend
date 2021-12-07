c78dcd0686c825cc2047b5790dfe47165b9a4aba
combiJihoon <combi_jihoon@naver.com>
2021-12-07 14:19:30 +0900

    feat: Create a permission for admin userr to delete registered users with token_blacklist.admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from rest_framework_simplejwt.token_blacklist import models
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
+
 from .models import User
 
-# Register your models here.
+
+class NewOutstandingTokenAdmin(OutstandingTokenAdmin):
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+
+admin.site.unregister(models.OutstandingToken)
+admin.site.register(models.OutstandingToken, NewOutstandingTokenAdmin)
+
 admin.site.register(User)
