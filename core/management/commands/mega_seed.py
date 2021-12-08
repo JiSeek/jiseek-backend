57e019268048699b027979a535080536c792d203
jh <kojh0111@naver.com>
2021-12-08 14:20:17 +0900

    feat: Seed(Boards)

--- core/management/commands/mega_seed.py
+++ core/management/commands/mega_seed.py
@@ -2,7 +2,9 @@ import random
 from django.core.management.base import BaseCommand
 from users.models import User
 from mypage.models import Profile
+from boards.models import Board
 from django_seed import Seed
+from faker import Faker
 
 
 class Command(BaseCommand):
@@ -31,3 +33,14 @@ class Command(BaseCommand):
             profile.image = f"media/default/{random.randint(1,7)}.png"
             profile.save()
         self.stdout.write(self.style.SUCCESS("Profile seeded"))
+
+        users = User.objects.all()
+
+        for user in users:
+            for _ in range(random.randint(0, 3)):
+                Board.objects.create(
+                    user=user,
+                    content=Faker().sentence(nb_words=6),
+                    photo=f"media/0_board/{random.randint(1,11)}.jpg",
+                )
+        self.stdout.write(self.style.SUCCESS("Boards seeded"))
