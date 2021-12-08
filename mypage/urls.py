5e0c075d8e97254b66a84c48d045d472b90ba9e2
combiJihoon <combi_jihoon@naver.com>
2021-12-08 22:17:56 +0900

    feat: Create myboards api to check all the boards a user created

--- mypage/urls.py
+++ mypage/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("board-favs/", views.BoardFavsAPI.as_view()),
+    path("myboards/", views.MyBoardsAPI.as_view()),
     path("food-favs/", views.FoodFavsAPI.as_view()),
     path("board/like/<int:pk>/", views.like_board),
     path("food/like/<int:pk>/", views.like_food),
