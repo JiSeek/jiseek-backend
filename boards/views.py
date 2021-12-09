7cd1da4f5ac40a30c74094057b73a63970ebbf9e
combiJihoon <combi_jihoon@naver.com>
2021-12-09 18:46:18 +0900

    debug: Remove unnecesary method post in boardsview

--- boards/views.py
+++ boards/views.py
@@ -42,15 +42,6 @@ class BoardsView(ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return JsonResponse(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
-
 
 class BoardView(RetrieveUpdateDestroyAPIView):
     """
